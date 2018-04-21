465cd1b3d35710ef14b48ea3e7354611d6c737b3
qiqiboy <qiqiboy@tigerbrokers.com>
2018-04-22 02:32:26 +0800

    fix an annotation error

--- AnimatedRouter.js
+++ AnimatedRouter.js
@@ -30,7 +30,7 @@ const isHistoryPush = location => {
  *
  *  需要动画样式文件配合，所以如果使用默认的动画效果，则需要一并将项目中的animated.css导入项目
  *  import AnimatedRouter from 'react-animated-router';
- *  import 'react-animated-router/animated.css';
+ *  import 'react-animated-router/animate.css';
  */
 @withRouter
 class AnimatedRouter extends Component {
