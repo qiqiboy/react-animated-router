b05147f8e7ca90e17c25983d401071e2d73160a1
qiqiboy <imqiqiboy@gmail.com>
2022-06-01 19:52:17 +0800

    Update AnimatedRoutes.tsx

--- src/AnimatedRoutes.tsx
+++ src/AnimatedRoutes.tsx
@@ -11,7 +11,6 @@ export { useAnimatedRoutes };
  * @author qiqiboy
  *
  *  需要动画样式文件配合，所以如果使用默认的动画效果，则需要一并将项目中的animated.css导入项目
- *  import AnimatedRouter from 'react-animated-router';
  *  import 'react-animated-router/animate.css';
  */
 export const AnimatedRoutes: React.FC<
